602583f559fcf87616bded0d542d44081aa95f37
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-10-10 13:26:25 -0300

    Added Docker for PostgreSQL && application Properties && Select Queries for DB
    Added Docker compose file for PostgreSQL-Alpine container configuration && altered the application.properties file with the connection configurations for the PostgreSQL DB && added the SELECT queries inside BirdDataAccessService that show all birds and the one that shows only a single bird.

--- src/main/java/dbserver/wingedinsight/dao/BirdDataAccessService.java
+++ src/main/java/dbserver/wingedinsight/dao/BirdDataAccessService.java
@@ -1,6 +1,8 @@
 package dbserver.wingedinsight.dao;
 
 import dbserver.wingedinsight.model.Bird;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,7 +10,15 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository("postgres")
-public class BirdDataAccessService implements BirdDao{
+public class BirdDataAccessService implements BirdDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public BirdDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertBird(UUID id, Bird bird) {
         return 0;
@@ -16,12 +26,23 @@ public class BirdDataAccessService implements BirdDao{
 
     @Override
     public List<Bird> selectAllBirds() {
-        return List.of(new Bird(UUID.randomUUID(), "FROM POSTGRESS DB"));
+        final String sql = "SELECT id, name FROM bird";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Bird(id, name);
+        });
     }
 
     @Override
     public Optional<Bird> selectBirdById(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id, name FROM bird WHERE id = ?";
+        Bird bird = jdbcTemplate.queryForObject(sql, new Object[]{id}, (resultSet, i) -> {
+            UUID birdId = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Bird(birdId, name);
+        });
+        return Optional.ofNullable(bird);
     }
 
     @Override
---
+++ src/main/resources/db/migration/V1__CreateBirdTable.sql
@@ -0,0 +1,4 @@
+CREATE TABLE bird (
+    id UUID NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
