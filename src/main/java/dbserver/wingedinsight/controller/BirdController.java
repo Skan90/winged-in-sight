9dedb16ca449f15b739fd5e4c3ebf891bc595bf3
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-10-27 03:55:24 -0300

    Update BirdController.java

--- src/main/java/dbserver/wingedinsight/controller/BirdController.java
+++ src/main/java/dbserver/wingedinsight/controller/BirdController.java
@@ -12,7 +12,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin("*")
+
 @RestController
 @RequestMapping("api/v1/bird")
 public class BirdController {
