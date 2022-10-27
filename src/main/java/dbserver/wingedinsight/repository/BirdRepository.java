e2aa6f677c523e561b3761eaad8ecd94f6b69a60
Rafael da Silva Santos <rafasilva.9021@gmail.com>
2022-10-27 04:03:19 -0300

    Update BirdRepository.java

--- src/main/java/dbserver/wingedinsight/repository/BirdRepository.java
+++ src/main/java/dbserver/wingedinsight/repository/BirdRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-@Repository("postgresql")
+@Repository
 public interface BirdRepository extends JpaRepository<Bird, Integer> {
 
     Optional<Bird> findBySpecies(String species);
